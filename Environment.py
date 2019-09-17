3874330c20809d167bc56cf32b1080ae948e8fde
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-09-17 17:03:22 +0200

    Feat : addAgent
    Ajout d'un agent dans l'espace. Lève une exception si la case est déjà prise

--- Environment.py
+++ Environment.py
@@ -1,9 +1,17 @@
+import numpy as np
+
+import Agent
+
 class Environment:
 
     def __init__(self, width=100, height=100, isToric=False):
-        self.width = width
-        self.height = height
+        self.space = np.empty((width, height), dtype=Agent.Agent , order='F')
         self.isToric = isToric
         self.agents = []
     
-    
+    def addAgent(self, agent, x, y):
+        if self.space[y][x] == None:
+            self.space[y][x] = agent
+            self.agents.append(agent)
+        else:
+            raise ValueError("The position is already taken")
