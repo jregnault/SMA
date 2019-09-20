ea3c5deb420daf438d02133c1d6781b94c21fcd2
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-09-20 12:59:16 +0200

    organisation fichier conf

--- config.py
+++ config.py
@@ -1,21 +1,25 @@
 import configparser
 
 config = configparser.ConfigParser()
-config['settings'] = {
-    'torus':False,
-    'gridSizeX':100,
-    'gridSizeY':100,
-    'canvasSizeX':1000,
-    'canvasSizeY':1000,
-    'boxSize':10,
+config['simulation'] = {
     'delay':10,
     'scheduling':"fair",
     'nbTicks':0,
-    'grid':True,
     'trace':True,
     'seed':0,
-    'refresh':1,
-    'nbParticles':100
+    'nbParticles':100,
+    'refresh':1
+}
+config['view'] = {
+    'canvasSizeX':1000,
+    'canvasSizeY':1000,
+    'boxSize':10,
+    'grid':True
+}
+config['env'] = {
+    'torus':False,
+    'gridSizeX':100,
+    'gridSizeY':100
 }
 with open("config.ini","w") as configfile:
     config.write(configfile)
