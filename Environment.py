f7083466b4b4308b7c681a6966cb041dc4992af7
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-09-23 15:11:57 +0200

    feat : getNeighborhood

--- Environment.py
+++ Environment.py
@@ -21,6 +21,27 @@ class Environment:
     def getAgent(self, x, y):
         return self.space[x][y]
     
+    def getNeighborhood(self, agent, distance):
+        neighbors = []
+        if self.torus:
+            wmin = (agent.posX - distance) % self.getWidth()
+            wmax = (agent.posX + distance) % self.getWidth()
+            hmin = (agent.posY - distance) % self.getHeight()
+            hmax = (agent.posY + distance) % self.getHeight()
+        else:
+            wmin = max(0,agent.posX - distance)
+            wmax = min(self.getWidth()-1,agent.posX + distance)
+            hmin = max(0,agent.posY - distance)
+            hmax = min(self.getHeight()-1,agent.posY + distance)
+        
+        for y in range(hmin,hmax+1):
+            for x in range(wmin,wmax+1):
+                a = self.getAgent(x,y)
+                if a != None:
+                    neighbors.append(a)
+        
+        return neighbors
+
     def isEmpty(self, x, y):
         return self.space[x][y] == None
 
