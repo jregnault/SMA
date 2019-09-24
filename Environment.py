c34614aca2f5f1a1524e655deb7306ebf571a583
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-09-24 16:26:43 +0200

    feat : env tools

--- Environment.py
+++ Environment.py
@@ -10,38 +10,18 @@ class Environment:
         self.height = gridSizeY
         self.space = np.empty((gridSizeX, gridSizeY), dtype=Agent.Agent , order='F')
         self.torus = torus
-        self.agents = []
     
-    def addAgent(self, agent, x, y):
-        if self.space[y][x] == None:
-            self.space[y][x] = agent
-            self.agents.append(agent)
+    def add(self, agent, x, y):
+        if self.space[x][y] == None:
+            self.space[x][y] = agent
         else:
-            self.addAgent(agent,random.randint(0,self.width()-1),random.randint(0,self.height()-1))
+            raise ValueError("Add : the cell is already taken !")
     
-    def getAgent(self, x, y):
+    def remove(self, x, y):
+        self.space[x][y] = None
+
+    def get(self, x, y):
         return self.space[x][y]
     
-    def getNeighborhood(self, agent, distance):
-        neighbors = []
-        if self.torus:
-            wmin = (agent.posX - distance) % self.width()
-            wmax = (agent.posX + distance) % self.width()
-            hmin = (agent.posY - distance) % self.height()
-            hmax = (agent.posY + distance) % self.height()
-        else:
-            wmin = max(0,agent.posX - distance)
-            wmax = min(self.width()-1,agent.posX + distance)
-            hmin = max(0,agent.posY - distance)
-            hmax = min(self.height()-1,agent.posY + distance)
-        
-        for y in range(hmin,hmax+1):
-            for x in range(wmin,wmax+1):
-                a = self.getAgent(x,y)
-                if a != None:
-                    neighbors.append(a)
-        
-        return neighbors
-
     def isEmpty(self, x, y):
         return self.space[x][y] == None
