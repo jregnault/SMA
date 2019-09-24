e677527d9511435814068da80e773991d5c92bfa
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-09-24 15:55:57 +0200

    config

--- Agent.py
+++ Agent.py
@@ -14,22 +14,26 @@ class Agent:
         self.isBouncing = False
     
     def update(self):
-        pass
+        if self.isBouncing:
+            self.color = (250,0,0,0)
+        else:
+            self.color = (250,250,250,0)
 
     def decide(self):
-        neighbors = self.environment.getNeighborhood(self,2)
-        if neighbors == []:
-            
-        if 0 <= (self.posX + self.stepX) < self.environment.getWidth() and 0 <= (self.posY + self.stepY) < self.environment.getHeight():
-            if self.environment.isEmpty(self.posX + self.stepX, self.posY + self.stepY):
-                self.isBouncing = False
-                self.move()
-            else:
-                self.agentBounce(self.environment.getAgent(self.posX + self.stepX, self.posY + self.stepY))
+        neighbors = self.environment.getNeighborhood(self,1)
+        if neighbors != []:
+            self.move()
+            for n in neighbors:
+                if (n.posX + n.stepX) == (self.posX + self.stepX) and (n.posY + n.stepY) == (self.posY + self.stepY):
+                    self.agentBounce(n)
+        elif 0 <= (self.posX + self.stepX) < self.environment.getWidth() and 0 <= (self.posY + self.stepY) < self.environment.getHeight():
+            self.isBouncing = False
+            self.move()
         else:
             self.wallBounce()
 
     def move(self):
+        #self.environment.moveAgent(self)
         self.posX += self.stepX
         self.posY += self.stepY
 
@@ -40,9 +44,13 @@ class Agent:
             self.stepX *= -1
         if self.posY == 0 or self.posY == self.environment.getHeight() -1:
             self.stepY *= -1
+        self.move()
     
     def agentBounce(self, agent):
         self.isBouncing = True
         self.color = (250,0,0,0)
         self.stepX, agent.stepX = agent.stepX, self.stepX
         self.stepY, agent.stepY = agent.stepY, self.stepY
+        self.move()
+        agent.isBouncing = True
+        agent.update()
