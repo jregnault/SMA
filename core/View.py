6fec3251d948a3d33d6229b1efda2728b932fa14
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-10-13 15:53:36 +0200

    feat : drawGrid

--- core/View.py
+++ core/View.py
@@ -12,7 +12,28 @@ class View:
         if grid:
             canvasSizeX += environment.gridSizeX + 1
             canvasSizeY += environment.gridSizeY + 1
-            
+
         pygame.init()
         self.screen = pygame.display.set_mode((canvasSizeX,canvasSizeY))
         pygame.display.set_caption("Multi-Agent System")
+        self.screen.fill(backgroundColor)
+        if grid:
+            self.drawGrid()
+    
+    def drawGrid(self):
+        """Add a grid to the view."""
+        gridSurface = pygame.Surface(self.screen.get_size())
+        gridSurface = gridSurface.convert_alpha()
+        gridSurface.fill(0,0,0,0)
+
+        width = gridSurface.get_width()
+        height = gridSurface.get_height()
+        for i in range(0, self.environement.width+1):
+            pos = i * self.boxSize + i
+            pygame.draw.line(gridSurface, (0,0,0,255), (pos,0), (pos,height-1))
+        
+        for j in range(0, self.environement.height+1):
+            pos = j * self.boxSize + j
+            pygame.draw.line(gridSurface, (0,0,0,255), (0,pos), (width-1,pos))
+        
+        self.screen.blit(gridSurface)
