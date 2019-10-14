f5e63df1da48af0eda9d096511de17cf8f6a3518
Jérémie Regnault <jeremie.regnault.etu@univ-lille.fr>
2019-10-14 12:02:34 +0200

    feat : init WaTor

---
+++ wator/WaTorSMA.py
@@ -0,0 +1,44 @@
+import random
+
+from wator.Fish import Fish
+from wator.Shark import Shark
+
+class WaTorSMA:
+    def __init__(self, environment, view, scheduling, trace, nbFishes, fishBreedTime, nbSharks, sharkBreedTime, sharkStarveTime):
+        self.environment = environment
+        self.view = view
+        self.scheduling = scheduling
+        self.trace = trace
+
+        self.agentList = []
+        self.birthList = []
+        self.observers = []
+
+        self.nextAgentId = 0
+
+        for _ in range(0, nbFishes):
+            self.agentList.append(
+                Fish(
+                    self.nextAgentId,
+                    environment,
+                    random.randint(0,environment.width - 1),
+                    random.randint(0,environment.height - 1),
+                    (255,255,0,255),
+                    fishBreedTime
+                )
+            )
+            self.nextAgentId += 1
+        
+        for _ in range(0, nbSharks):
+            self.agentList.append(
+                Shark(
+                    self.nextAgentId,
+                    environment,
+                    random.randint(0,environment.width - 1),
+                    random.randint(0, environment.height - 1),
+                    (253,108,158,255),
+                    sharkBreedTime,
+                    sharkStarveTime
+                )
+            )
+            self.nextAgentId += 1
