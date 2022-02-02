aeb70884157793f729ced3c9727ce42f462a9d32
Jérémie Regnault <jeremie.regnault@capgemini.com>
2022-02-02 10:54:22 +0100

    refactor : PEP-8
    PEP-8 applied on View.py
    
    Warning: Particles sim broken (WIP)

--- core/View.py
+++ core/View.py
@@ -1,58 +1,58 @@
 import pygame
-from pygame.locals import *
+
 
 class View:
     
-    def __init__(self, environment, boxSize=1, grid=False):
+    def __init__(self, environment, box_size=1, grid=False):
         self.environment = environment
-        self.boxSize = boxSize
+        self.boxSize = box_size
         self.grid = grid
 
-        canvasSizeX = environment.width * boxSize
-        canvasSizeY = environment.height * boxSize
+        canvas_size_x = environment.width * box_size
+        canvas_size_y = environment.height * box_size
         if grid:
-            canvasSizeX += environment.width + 1
-            canvasSizeY += environment.height + 1
+            canvas_size_x += environment.width + 1
+            canvas_size_y += environment.height + 1
 
         pygame.init()
-        self.screen = pygame.display.set_mode((canvasSizeX,canvasSizeY))
+        self.screen = pygame.display.set_mode((canvas_size_x, canvas_size_y))
         pygame.display.set_caption("Multi-Agent System")
-        self.drawBackground()
+        self.draw_background()
         if grid:
-            self.drawGrid()
+            self.draw_grid()
     
-    def drawBackground(self):
+    def draw_background(self):
         background = pygame.Surface(self.screen.get_size())
         background = background.convert()
         background.fill(self.environment.color)
-        self.screen.blit(background,(0,0))
+        self.screen.blit(background, (0, 0))
 
-    def drawGrid(self):
+    def draw_grid(self):
         """Add a grid to the view."""
-        gridSurface = pygame.Surface(self.screen.get_size())
-        gridSurface = gridSurface.convert()
-        gridSurface.fill(self.environment.color)
+        grid_surface = pygame.Surface(self.screen.get_size())
+        grid_surface = grid_surface.convert()
+        grid_surface.fill(self.environment.color)
 
-        width = gridSurface.get_width()
-        height = gridSurface.get_height()
+        width = grid_surface.get_width()
+        height = grid_surface.get_height()
         for i in range(0, self.environment.width+1):
             pos = i * self.boxSize + i
-            pygame.draw.line(gridSurface, (0,0,0,255), (pos,0), (pos,height-1))
+            pygame.draw.line(grid_surface, (0, 0, 0, 255), (pos, 0), (pos, height-1))
         
         for j in range(0, self.environment.height+1):
             pos = j * self.boxSize + j
-            pygame.draw.line(gridSurface, (0,0,0,255), (0,pos), (width-1,pos))
+            pygame.draw.line(grid_surface, (0, 0, 0, 255), (0, pos), (width-1, pos))
         
-        self.screen.blit(gridSurface,(0,0))
+        self.screen.blit(grid_surface, (0, 0))
     
     def update(self, observable):
         if self.grid:
-            self.drawGrid()
+            self.draw_grid()
         else:
-            self.drawBackground()
-        for y in range(0,self.environment.height):
+            self.draw_background()
+        for y in range(0, self.environment.height):
             for x in range(0, self.environment.width):
                 agent = self.environment.get(x, y)
-                if agent != None:
+                if agent is not None:
                     agent.draw(self)
         pygame.display.flip()
