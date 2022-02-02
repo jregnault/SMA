fc0e96e5717e65f5ebc9a292452ba9de070d0086
Jérémie Regnault <jeremie.regnault@capgemini.com>
2022-02-02 11:47:53 +0100

    refactor : PEP-8
    PEP-8 applied on config.py

--- config.py
+++ config.py
@@ -2,24 +2,24 @@ import configparser
 
 config = configparser.ConfigParser()
 config['simulation'] = {
-    'delay':10,
-    'scheduling':"fair",
-    'nbTicks':0,
-    'trace':True,
-    'seed':0,
-    'nbParticles':100,
-    'refresh':1
+    'delay': 10,
+    'scheduling': "fair",
+    'nbTicks': 0,
+    'trace': True,
+    'seed': 0,
+    'nbParticles': 100,
+    'refresh': 1
 }
 config['view'] = {
-    'canvasSizeX':1000,
-    'canvasSizeY':1000,
-    'boxSize':10,
-    'grid':True
+    'canvasSizeX': 1000,
+    'canvasSizeY': 1000,
+    'boxSize': 10,
+    'grid': True
 }
 config['env'] = {
-    'torus':False,
-    'gridSizeX':100,
-    'gridSizeY':100
+    'torus': False,
+    'gridSizeX': 100,
+    'gridSizeY': 100
 }
-with open("config.ini","w") as configfile:
+with open("config.ini", "w") as configfile:
     config.write(configfile)
